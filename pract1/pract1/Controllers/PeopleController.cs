9800df1e48c0579877bfe8d8f32ccb66c058b9d2
Dharmateja Kandakatla <dharmatejakandakatla555@gmail.com>
2021-02-17 20:56:05 -0800

    test

---
+++ pract1/pract1/Controllers/PeopleController.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using pract1.Data;
+using pract1.Models;
+
+namespace pract1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PeopleController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public PeopleController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/People
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<People>>> Getpeoples()
+        {
+            return await _context.peoples.ToListAsync();
+        }
+
+        // GET: api/People/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<People>> GetPeople(int id)
+        {
+            var people = await _context.peoples.FindAsync(id);
+
+            if (people == null)
+            {
+                return NotFound();
+            }
+
+            return people;
+        }
+
+        // PUT: api/People/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutPeople(int id, People people)
+        {
+            if (id != people.AutoId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(people).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PeopleExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/People
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<People>> PostPeople(People people)
+        {
+            _context.peoples.Add(people);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetPeople", new { id = people.AutoId }, people);
+        }
+
+        // DELETE: api/People/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<People>> DeletePeople(int id)
+        {
+            var people = await _context.peoples.FindAsync(id);
+            if (people == null)
+            {
+                return NotFound();
+            }
+
+            _context.peoples.Remove(people);
+            await _context.SaveChangesAsync();
+
+            return people;
+        }
+
+        private bool PeopleExists(int id)
+        {
+            return _context.peoples.Any(e => e.AutoId == id);
+        }
+    }
+}
---
+++ pract1/pract1/Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using pract1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace pract1.Data
+{
+    public class AppDbContext : DbContext
+
+    {
+        public AppDbContext( DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        public DbSet<People>peoples { get; set; }
+    }
+}
